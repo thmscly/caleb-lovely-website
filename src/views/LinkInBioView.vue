b2467bbc9670875696bdc0ff1a31138314d70457
T.C. Cox <thmscly@gmail.com>
2025-03-14 13:15:03 -0400

    refactor: styled link-in-bio

--- src/views/LinkInBioView.vue
+++ src/views/LinkInBioView.vue
@@ -11,20 +11,25 @@ import { Icon } from '@iconify/vue'
       <div class="social-links">
         <a href="https://www.instagram.com/caleblovely/" target="_blank" class="link">
           <Icon icon="mdi:instagram" class="icon" />
+          <h2>INSTAGRAM</h2>
         </a>
         <a href="https://www.tiktok.com/@caleblovelymusic" target="_blank" class="link">
           <Icon icon="simple-icons:tiktok" class="icon" />
+          <H2>TIKTOK</H2>
         </a>
         <a href="https://open.spotify.com/artist/588OniOrJFEKU5hWUDJ6V7?si=7yg3ubdyRSynaaxH2mZH7A" target="_blank"
           class="link">
           <Icon icon="mdi:spotify" class="icon" />
+          <h2>SPOTIFY</h2>
         </a>
         <a href="https://open.spotify.com/artist/588OniOrJFEKU5hWUDJ6V7?si=7yg3ubdyRSynaaxH2mZH7A" target="_blank"
           class="link">
           <Icon icon="simple-icons:applemusic" class="icon" />
+          <h2>APPLE MUSIC</h2>
         </a>
         <a href="https://www.youtube.com/@calovely" target="_blank" class="link">
           <Icon icon="mdi:youtube" class="icon" />
+          <h2>YOUTUBE</h2>
         </a>
       </div>
     </section>
@@ -52,6 +57,7 @@ import { Icon } from '@iconify/vue'
   transition: all 0.3s ease;
   text-decoration: none;
   height: 5em;
+  gap: 1rem;
 }
 
 .link:hover {
@@ -63,5 +69,31 @@ import { Icon } from '@iconify/vue'
   width: 35px;
   height: 35px;
   color: white;
+}
+
+h2 {
+  color: white;
+  margin: 0;
+  display: none;
+}
+
+@media screen and (min-width: 768px) {
+  h2 {
+    display: block;
+  }
+
+  .social-links {
+    width: 50vw;
+  }
+}
+
+@media screen and (max-width: 767px) {
+  .social-links {
+    width: 90vw;
+  }
+
+  .link {
+    height: 3.5em;
+  }
 }
 </style>
